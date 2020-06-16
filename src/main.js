4bfa1b3d0bf36f6c8865bc90b8a377e9d2c186b3
luucamay <lupemaydana@gmail.com>
2020-06-16 05:55:42 -0400

    style: Remove example pokemon card to use dynamic data

--- src/main.js
+++ src/main.js
@@ -3,5 +3,35 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-// console.log(data);
-pokemonQuantity.textContent = data.pokemon.length;
+console.log(example);
+
+const pokemonArray = data.pokemon;
+const pokemonQuantity = document.querySelector('#pokemonQuantity');
+const mainView = document.querySelector('main');
+
+const renderMainView = () => {
+  const pokemonCards = document.createElement('div');
+  pokemonCards.classList.add('pokemon-cards');
+
+  pokemonArray.forEach(element => {
+    //console.log(element);
+    const pokemonCard = document.createElement('div');
+    pokemonCard.classList.add('pokemon-card');
+    const pokemonImg = document.createElement('img');
+    const pokemonName = document.createElement('p');
+    const pokemonType = document.createElement('p');
+    pokemonCard.appendChild(pokemonImg);
+    pokemonCard.appendChild(pokemonName);
+    pokemonCard.appendChild(pokemonType);
+    pokemonImg.src = element.img;
+    pokemonName.textContent = element.name;
+    pokemonType.textContent = element.type.join(' | ');
+    pokemonCards.appendChild(pokemonCard);
+  });
+
+  mainView.appendChild(pokemonCards);
+
+}
+
+pokemonQuantity.textContent = pokemonArray.length;
+renderMainView();
